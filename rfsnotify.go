bb092ed3cb2f11111c9c193f3898afefff2ec2f3
hongy <hongy3025@163.com>
2018-01-11 02:12:04 +0800

    my mod

--- rfsnotify.go
+++ rfsnotify.go
@@ -2,15 +2,18 @@
 package rfsnotify
 
 import (
-	"gopkg.in/fsnotify.v1"
-
 	"errors"
 	"os"
 	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
 )
 
-// RWatcher wraps fsnotify.Watcher. When fsnotify adds recursive watches, you should be able to switch your code to use fsnotify.Watcher
-type RWatcher struct {
+// DirFilter ...
+type DirFilter func(path string, info os.FileInfo) bool
+
+// Watcher wraps fsnotify.Watcher. When fsnotify adds recursive watches, you should be able to switch your code to use fsnotify.Watcher
+type Watcher struct {
 	Events chan fsnotify.Event
 	Errors chan error
 
@@ -20,90 +23,90 @@ type RWatcher struct {
 }
 
 // NewWatcher establishes a new watcher with the underlying OS and begins waiting for events.
-func NewWatcher() (*RWatcher, error) {
+func NewWatcher() (*Watcher, error) {
 	fsWatch, err := fsnotify.NewWatcher()
 	if err != nil {
 		return nil, err
 	}
 
-	m := &RWatcher{}
-	m.fsnotify = fsWatch
-	m.Events = make(chan fsnotify.Event)
-	m.Errors = make(chan error)
-	m.done = make(chan struct{})
+	w := &Watcher{}
+	w.fsnotify = fsWatch
+	w.Events = make(chan fsnotify.Event)
+	w.Errors = make(chan error)
+	w.done = make(chan struct{})
 
-	go m.start()
+	go w.start()
 
-	return m, nil
+	return w, nil
 }
 
 // Add starts watching the named file or directory (non-recursively).
-func (m *RWatcher) Add(name string) error {
-	if m.isClosed {
+func (w *Watcher) Add(name string) error {
+	if w.isClosed {
 		return errors.New("rfsnotify instance already closed")
 	}
-	return m.fsnotify.Add(name)
+	return w.fsnotify.Add(name)
 }
 
 // AddRecursive starts watching the named directory and all sub-directories.
-func (m *RWatcher) AddRecursive(name string) error {
-	if m.isClosed {
+func (w *Watcher) AddRecursive(name string, dirFilter DirFilter) error {
+	if w.isClosed {
 		return errors.New("rfsnotify instance already closed")
 	}
-	if err := m.watchRecursive(name, false); err != nil {
+	if err := w.watchRecursive(name, false, dirFilter); err != nil {
 		return err
 	}
 	return nil
 }
 
 // Remove stops watching the the named file or directory (non-recursively).
-func (m *RWatcher) Remove(name string) error {
-	return m.fsnotify.Remove(name)
+func (w *Watcher) Remove(name string) error {
+	return w.fsnotify.Remove(name)
 }
 
 // RemoveRecursive stops watching the named directory and all sub-directories.
-func (m *RWatcher) RemoveRecursive(name string) error {
-	if err := m.watchRecursive(name, true); err != nil {
+func (w *Watcher) RemoveRecursive(name string) error {
+	if err := w.watchRecursive(name, true, nil); err != nil {
 		return err
 	}
 	return nil
 }
 
 // Close removes all watches and closes the events channel.
-func (m *RWatcher) Close() error {
-	if m.isClosed {
+func (w *Watcher) Close() error {
+	if w.isClosed {
 		return nil
 	}
-	close(m.done)
-	m.isClosed = true
+	close(w.done)
+	w.isClosed = true
 	return nil
 }
 
-func (m *RWatcher) start() {
+func (w *Watcher) start() {
 	for {
 		select {
 
-		case e := <-m.fsnotify.Events:
+		case e := <-w.fsnotify.Events:
 			s, err := os.Stat(e.Name)
 			if err == nil && s != nil && s.IsDir() {
 				if e.Op&fsnotify.Create != 0 {
-					m.watchRecursive(e.Name, false)
+					w.watchRecursive(e.Name, false, nil)
 				}
 			}
-			//Can't stat a deleted directory, so just pretend that it's always a directory and
-			//try to remove from the watch list...  we really have no clue if it's a directory or not...
+			// Can't stat a deleted directory, so just pretend that it's always a directory and
+			// try to remove from the watch list...  we really have no clue if it's a directory or not...
 			if e.Op&fsnotify.Remove != 0 {
-				m.fsnotify.Remove(e.Name)
+				w.fsnotify.Remove(e.Name)
 			}
-			m.Events <- e
+			w.Events <- e
 
-		case e := <-m.fsnotify.Errors:
-			m.Errors <- e
+		case e := <-w.fsnotify.Errors:
+			w.Errors <- e
 
-		case <-m.done:
-			m.fsnotify.Close()
-			close(m.Events)
-			close(m.Errors)
+		case <-w.done:
+			w.fsnotify.Close()
+			close(w.Events)
+			close(w.Errors)
 			return
 		}
 	}
@@ -111,18 +114,23 @@ func (m *RWatcher) start() {
 
 // watchRecursive adds all directories under the given one to the watch list.
 // this is probably a very racey process. What if a file is added to a folder before we get the watch added?
-func (m *RWatcher) watchRecursive(path string, unWatch bool) error {
+func (w *Watcher) watchRecursive(path string, unWatch bool, dirFilter DirFilter) error {
 	err := filepath.Walk(path, func(walkPath string, fi os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if fi.IsDir() {
 			if unWatch {
-				if err = m.fsnotify.Remove(walkPath); err != nil {
+				if err = w.fsnotify.Remove(walkPath); err != nil {
 					return err
 				}
 			} else {
-				if err = m.fsnotify.Add(walkPath); err != nil {
+				if dirFilter != nil {
+					if !dirFilter(walkPath, fi) {
+						return filepath.SkipDir
+					}
+				}
+				if err = w.fsnotify.Add(walkPath); err != nil {
 					return err
 				}
 			}
